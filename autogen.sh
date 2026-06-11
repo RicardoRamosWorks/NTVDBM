36c58bdfd1b6e2c967fe7d7deeb2bdc1fe8e3761
Ricardo Ramos <70118189+RicardoRamosWorks@users.noreply.github.com>
2026-06-11 14:00:00 +0000

    1.0

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 export LDFLAGS="-static-libgcc -static-libstdc++ -mwindows -Wl,--gc-sections,-s"
 export CFLAGS="-D_WIN32_WINNT=0x0501 -D_WIN32_IE=0x0600 -DNTDDI_VERSION=0x05010000 -msse2 -march=pentium-m -mtune=pentium-m -O2 -pipe -fomit-frame-pointer -fno-strict-aliasing -mfpmath=sse -fno-unwind-tables -fno-asynchronous-unwind-tables -falign-functions=16 -falign-loops=16 -mpreferred-stack-boundary=4 -flto"
 export CXXFLAGS="$CFLAGS"
-LDFLAGS="-static-libgcc -static-libstdc++ -mwindows -flto -fuse-linker-plugin -Wl,--gc-sections,-s,--undefined=SDL_main"
+export LDFLAGS="-static-libgcc -static-libstdc++ -mwindows -flto -fuse-linker-plugin -Wl,--gc-sections,-s,--undefined=SDL_main"
 
 echo "Generating build information using aclocal, autoheader, automake and autoconf"
 echo "This may take a while ..."
